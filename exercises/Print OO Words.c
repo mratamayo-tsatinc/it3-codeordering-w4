dc4c1f72d4505c4261a7e95aa54f1739b3a33aac
Mark Robert <143669058+mratamayo-tsatinc@users.noreply.github.com>
2026-08-13 09:57:22 +0800

    Add Print OO Words program
    Implement basic structure to print predefined words.

---
+++ exercises/Print OO Words.c
@@ -0,0 +1,30 @@
+/*
+ * Basic Structure and Printing
+ * Sample Output:
+ * MOO
+ * FOO
+ * DOO
+ * ZOO
+ * 
+ * COO
+ * FOO
+ * FOO
+ *
+ * LOO
+ *
+ */
+
+#include <stdio.h>
+
+int main() 
+{
+    printf("MOO\n");
+    printf("FOO\n");
+    printf("DOO\n");
+    printf("ZOO\n\n");
+    printf("COO\n");
+    printf("FOO\n");
+    printf("FOO\n\n");
+    printf("LOO\n");
+    return 0;
+}
